d597ff027db17af265fe5306331c87b5db9af76c
HarshdaSaxena <58638127+HarshdaSaxena@users.noreply.github.com>
2022-07-18 19:34:13 +0530

    Add files via upload

---
+++ harshdasGrowthFactor.h
@@ -0,0 +1,72 @@
+#ifndef ASTRO_STANDARD_GROWTH_FACTOR_H
+#define ASTRO_STANDARD_GROWTH_FACTOR_H
+
+#include "cosmologyBase.h"
+#include "abstractGrowthFactor.h"
+#include "gEffective.h"
+
+namespace astro
+{
+  /**
+   * \ingroup cosmologicalModel
+   *
+   * \class harshdasGrowthFactor
+   *
+   * \brief Class defining the standard cosmological growth factor
+   *
+   * This class defines the standard cosmological growth factor, i.e. for
+   * laws of gravity which scale with wave number as \f$k^{-2}\f$. The class
+   * is derived from the abstract class growthFactor.
+   *
+   * \author Matthias Bartelmann, Heidelberg University
+   */
+  class harshdasGrowthFactor: public abstractGrowthFactor
+  {
+  protected:
+    cosmologyBase * cosmological_model;
+    gEffective * g_effective;
+    bool g_eff_internal;
+    double k_internal;
+
+    /**
+     * Provides the differential equation to be solved
+     */
+    int differential_equation (double x, const double * y, double * dy);
+  public:
+    /**
+     * Constructor initializing the growth factor with a pointer to a
+     * cosmological model and optionally an effective gravitational
+     * constant.
+     * \param cosmological_model_in pointer to input cosmological model
+     * \param g_effective_in pointer to input model for effective
+     * gravitational constant
+     */
+    harshdasGrowthFactor
+      (cosmologyBase * cosmological_model_in,
+       gEffective * g_effective_in = NULL);
+
+    /**
+     * Destructor
+     */
+    ~harshdasGrowthFactor ();
+
+    /**
+     * Function returning the growth factor as a function of the scale
+     * factor \c a, divided by the scale factor,
+     * \f[
+     *   G_+(a,k) = \frac{D_+(a,k)}{a}\;.
+     * \f]
+     * \param a scale factor
+     * \param k wave number
+     */
+    double operator () (double a, double k = 1.0);
+
+    /**
+     * Returns the effective gravitational constant at scale factor \c a
+     * \param a scale factor
+     */
+    double gravitationalConstant (const double a, const double k);
+  };
+}
+
+#endif
